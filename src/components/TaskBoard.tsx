a6ecb8841d75ffbf06825c425647cc7f0c7453d3
Felizardo <felizardopaulo40@gmail.com>
2025-04-04 23:51:30 +0200

    Adiciona a API de tarefas

--- src/components/TaskBoard.tsx
+++ src/components/TaskBoard.tsx
@@ -4,33 +4,13 @@ import { TaskCard } from "./TaskCard"
 
 export const TaskBoard: React.FC = () => {
   const tasksTodo: Task[] = [
-    {
-      id: 2,
-      title: "Reunião com a equipe",
-      description:
-        "Reunião para discutir o progresso do projeto e próximos passos.",
-      status: "todo",
-      priority: "high",
-    },
+    
   ];
   const tasksInProgress: Task[] = [
-    {
-      id: 1,
-      title: "Enviar relatório",
-      description: "Enviar o relatório mensal para o departamento financeiro.",
-      status: "doing",
-      priority: "high",
-    },
+   
   ];
   const tasksDone: Task[] = [
-    {
-      id: 3,
-      title: "Atualizar o site",
-      description:
-        "Fazer atualizações no site da empresa com novas informações.",
-      status: "done",
-      priority: "medium",
-    },
+    
   ];
 
   return (
