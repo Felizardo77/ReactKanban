7c7188ed9cb24d8d484bfe41157cae9db06c9d0d
Felizardo <felizardopaulo40@gmail.com>
2025-04-04 18:53:11 +0200

    Primeiro commit

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import { Heading, Box, Flex } from "@radix-ui/themes";
+import { CreateTaskForm } from "./components/CreateTaskForm";
+export default function App() {
+  return (
+    <Box maxWidth="80rem" mx="auto">
+      <Box height="4rem">
+        <Flex align="center" gap="4" height="100%">
+          <Heading as="h1" size="8" weight="light">
+            React Kanban
+          </Heading>
+          <CreateTaskForm />
+        </Flex>
+      </Box>
+
+      <Box>
+        <Heading as="h2">Quadro de tarefas</Heading>
+      </Box>
+    </Box>
+  );
+}
---
+++ src/components/TaskBoard.tsx
@@ -0,0 +1,6 @@
+import { Task } from "../entities/Task"
+
+const TaskBoard : React.FC  = () =>{
+    const tasksTodo: Task[]
+    const tasksInProgress:Task[]
+}
